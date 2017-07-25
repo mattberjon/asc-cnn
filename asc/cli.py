78514cf456bf743bbd31874d6a919c1fb7d7cd17
Matthieu Berjon <matthieu@berjon.net>
2017-07-25 19:02:59 +0200

    update of cli file
    I updated the cli file in order to print the version of the program as
    well as the version of python.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- asc/cli.py
+++ asc/cli.py
@@ -3,9 +3,25 @@
 """Console script for asc."""
 
 import click
+import sys
 
+from . import __version__
 
-@click.command()
+def version_msg():
+    """ Returns the program version, location and python version.
+    """
+
+    python_version = sys.version[:3]
+    message = 'asc %(version)s (Python {})'
+    return message.format(python_version)
+
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.version_option(
+        __version__,
+        '-V',
+        '--version',
+        message=version_msg(),
+        help='Output the version of the application')
 def main(args=None):
     """Console script for asc."""
     click.echo("Replace this message by putting your code into "
