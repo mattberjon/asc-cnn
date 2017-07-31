b7c19f4156ab28c7379bd315e04559478757bc5d
Matthieu Berjon <matthieu@berjon.net>
2017-07-31 16:50:07 +0200

    Add a download method to the Data class
    I added a `download()` method to the `Data` class. The method display a progress
    bar according to the size of the file.
    I removed the function that were used before and clean a bit the code.
    
    I did a small correction as well to the `file_to_list()` method on the
    line splitting. The function were returning a nested list, it now
    returns a simple list.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- asc/data.py
+++ asc/data.py
@@ -18,58 +18,44 @@ class Data(object):
     def __init__(self):
         pass
 
-    def get_data(self):
-        pass
+    def file_to_list(self, filename):
+        lines = []
+        with open(filename) as f:
+            for line in f.readlines():
+                lines.append(line.split('\n')[0])
+        return lines
+
+    def download(self, url_list, dest_dir):
+        elem_nb = len(url_list)
+        counter = 0
+
+        for elem in url_list:
+            filename = elem.split('/')[-1]
+
+            with open(dest_dir + '/' + filename, 'wb') as f:
+                r = requests.get(elem, stream=True)
+                dl = 0
+                file_size = int(r.headers['Content-length'])
+                if file_size is None:
+                    f.write(r.conent)
+                else:
+                    counter += 1
+                    for chunk in r.iter_content(chunk_size=1024):
+                        dl += len(chunk)
+                        f.write(chunk)
+                        done = int(50 * dl / file_size)
+                        sys.stdout.write(
+                            "\r%s/%d [%s%s]" %
+                            (counter, elem_nb, '=' * done, ' ' * (50-done)))
+                        sys.stdout.flush()
+
+            print('')
 
     def unzip_data(self):
         pass
 
     def clear_zip(self):
         pass
-
-
-tmp_dir = '/tmp'
-dest_dir = 'data'
-
-urls = [
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.1.zip',
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.2.zip',
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.3.zip',
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.4.zip',
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.5.zip',
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.6.zip',
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.7.zip',
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.8.zip',
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.9.zip',
-    'https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-o.10.zip']
-
-
-def downloader(url, path):
-    elem_nb = len(url)
-    counter = 0
-
-    for elem in url:
-        filename = elem.split('/')[-1]
-
-        with open(path + '/' + filename, 'wb') as f:
-            r = requests.get(elem, stream=True)
-            dl = 0
-            file_size = int(r.headers['Content-length'])
-            if file_size is None:
-                f.write(r.conent)
-            else:
-                counter += 1
-                for chunk in r.iter_content(chunk_size=1024):
-                    dl += len(chunk)
-                    f.write(chunk)
-                    done = int(50 * dl / file_size)
-                    sys.stdout.write(
-                            "\r%s/%d [%s%s]" %
-                            (counter, elem_nb, '=' * done, ' ' * (50-done)))
-                    sys.stdout.flush()
-
-        print('')
-    return 1
 
 
 def unziper(filenames, origin_dir, dest_dir):
@@ -94,16 +80,16 @@ def cleaner(filenames, path):
         print("%s deleted" % elem)
 
 
-dlw = downloader(urls, tmp_dir)
-if dlw:
-    print('Download complete')
+#  dlw = downloader(urls, tmp_dir)
+#  if dlw:
+#      print('Download complete')
 
-filenames = []
-for elem in urls:
-    filenames.append(elem.split('/')[-1])
+#  filenames = []
+#  for elem in urls:
+#      filenames.append(elem.split('/')[-1])
 
-print('Unzipping of the files')
-unziper(filenames, tmp_dir, dest_dir)
-
-print('Cleaning of the downloaded files')
-cleaner(filenames, tmp_dir)
+#  print('Unzipping of the files')
+#  unziper(filenames, tmp_dir, dest_dir)
+#
+#  print('Cleaning of the downloaded files')
+#  cleaner(filenames, tmp_dir)
