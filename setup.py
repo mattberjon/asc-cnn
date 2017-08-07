0db07b6455da585b664a12be0c88efb64d9b57ca
Matthieu Berjon <matthieu@berjon.net>
2017-08-07 19:18:26 +0200

    Bump version: 0.2.2 → 0.3.0

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ test_requirements = [
 
 setup(
     name='asc',
-    version='0.2.2',
+    version='0.3.0',
     description="Acoustic Scene Classification using Convolutional Neural Network",
     long_description=readme + '\n\n' + history,
     author="Matthieu Berjon",
