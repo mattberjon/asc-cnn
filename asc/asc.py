4a134d280caf39fd958c7286b9dfa33bb8a86118
Matthieu Berjon <matthieu@berjon.net>
2017-08-12 15:48:57 +0200

    Added feature: Spectrogram calculation
    I added as a set a functions the spectrogram calculation. At the moment,
    the main function compute the spectrogram only of a sine signal
    according to its frequency, sampling rate and length in sample. The
    other function takes the output of spectrogram function to plot it.
    The next step would be to compute the spectrogram on a real signal given
    in a wav file.
    
    See also: #10
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- asc/asc.py
+++ asc/asc.py
@@ -1,3 +1,21 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+
+def spectrogram(frequency, sampling_rate, smp_len):
+    x = np.arange(smp_len)
+    sine = np.sin(2 * np.pi * frequency * x / sampling_rate)
+    f, t, Sxx = signal.spectrogram(sine, sampling_rate)
+    return f, t, Sxx
+
+
+def plot_spectrogram(f, t, Sxx):
+    plt.pcolormesh(t, f, Sxx)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [samples]')
+    plt.show()
