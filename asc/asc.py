209333f20a8f795d7a404780349c28c91f7f96d9
Matthieu Berjon <matthieu@berjon.net>
2017-08-16 14:36:42 +0200

    Added feature: dynamic spectrogram computation
    I added the computation of the dynamic spectrogram block by block.
    In order to perform this operation, I added several functions:
    
    - `spectrogram()` now computes the spectrogram for a real signal using
    the 3rd parties library Scipy. The documentation has been updated
    accordingly.
    - `plot_spectrogram()` now can either display the figure or save it. See
    the Todo part in the docstring to see the work that still need to be
    done on this particular part.
    - `audio_metadata()` extract some data from the audio file such as the
    samplerate or channel number in order to be able to compute the
    spectrogram on each channel properly. The name of the function isn't
    really self explanatory and will need either to be changed or be worked
    out to be divided in smaller functions.
    - `process_audio()` is an empty function having in the docstring
    information about the exact processing chain to respect to perform the
    computation correclty.
    
    Resolves: #10
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>
    
    Docstring fix
    
    I fixed the docstrings to match the exact denomination regarding the
    spectrogram computation. In the present code, we compute the dynamic
    spectrogram and not the static one that needs a different computation.
    
    See also: #10
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- asc/asc.py
+++ asc/asc.py
@@ -2,20 +2,106 @@
 
 """Main module."""
 
-import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
+from soundfile import SoundFile, blocks as sfblocks
 
 
-def spectrogram(frequency, sampling_rate, smp_len):
-    x = np.arange(smp_len)
-    sine = np.sin(2 * np.pi * frequency * x / sampling_rate)
-    f, t, Sxx = signal.spectrogram(sine, sampling_rate)
+def spectrogram(data, sampling_rate):
+    """ Compute the spectrogram of a time serie of samples.
+
+    The dynamic spectrogram is computed using the `spectrogram()` function
+    provided by Scipy.
+
+    Args:
+
+    Returns:
+        f: Array of sample frequencies
+        t: Array of segment times
+        Sxx: Spectrogram of the signal
+
+    Todo:
+        - be able to read the input of any signal given
+
+    """
+    f, t, Sxx = signal.spectrogram(data, sampling_rate)
     return f, t, Sxx
 
 
-def plot_spectrogram(f, t, Sxx):
+def plot_spectrogram(f, t, Sxx, plot=True, filename='nothing.png'):
+    """ Plot/Save the current figure.
+
+    Display or save the dynamic spectrogram according.
+
+    Args:
+        f: Array of sample frequencies
+        t: Array of segment times
+        Sxx: Spectrogram of the signal
+        plot: Flag to plot or save the figure
+
+    Todo:
+        - Save the filename to the data folder with the right name
+        - ensure to create each figure independently before showing or saving
+        them
+
+    """
     plt.pcolormesh(t, f, Sxx)
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [samples]')
-    plt.show()
+    if plot:
+        plt.show()
+    else:
+        plt.savefig(filename)
+
+
+def audio_metadata(filename, frame_size=9, display_plot=False):
+    """ extract audio metadata and compute the dynamic spectrogram.
+
+    Prepare the audio file and process it to compute the dynamic spectrograms
+    block by block.
+
+    Args:
+        filename (str): Path to the audio file.
+        frame_size (int): frame size for the "per block" processing.
+        display_plot (boolean): display or save the plot.
+
+    Returns:
+        Nothing
+
+    Todo:
+        - check if the samplerate of the file corresponds to the samplerate
+        in the configuration file
+
+    """
+    chunk = 512
+    audio_file = SoundFile(filename)
+    af_chan_nb = audio_file.channels
+    af_samplerate = audio_file.samplerate
+    
+    for block in sfblocks(filename, blocksize=chunk):
+        # separate the channels to compute the spectrograms
+        block_left = block[:, 0]
+        block_right = block[:, 1]
+        # Compute the dynamic spectrogram
+        f_left, t_left, Sxx_left = spectrogram(block_left, 44100)
+        f_right, t_right, Sxx_right = spectrogram(block_right, 44100)
+        # Plot the spectrogram
+        plot_spectrogram(f_left, t_left, Sxx_left, display_plot)
+        plot_spectrogram(f_right, t_right, Sxx_right, display_plot)
+
+
+def process_audio():
+    """ Process an audio file
+
+    Process the audio file and extract the metadata such as:
+
+    - load the audio file
+    - extract the right meta data (samplerate, channel nb, length of the file)
+    - check if the samplerate corresponds to the config file
+    - compute the right nb of frames to compute
+    - process the dynamic spectrogram
+    - process the static spectrogram
+    - save the images to the right folder
+
+    """
+    pass
