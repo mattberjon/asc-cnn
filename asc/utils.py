7d2f80583e94e8ed3af5af92a2814fcdef1f9adc
Matthieu Berjon <matthieu@berjon.net>
2017-08-21 21:24:38 +0200

    Convertion function: milliseconds to samples
    I wrote a small function converting milliseconds into samples.
    
    Resolves: #30
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- asc/utils.py
+++ asc/utils.py
@@ -71,3 +71,23 @@ def conf_param_extract(parameter):
     section = data[0]
     option = data[1]
     return section, option
+
+
+def ms2smp(ms, sample_rate):
+    """Milliseconds to samples converter.
+
+    Simple converter in order to compute the number of samples for a given
+    time frame in milliseconds and the sampling rate.
+
+    Args:
+
+        ms (int):           Number of milliseconds.
+        sample_rate (int):  Sampling rate in Hertz.
+
+    Returns:
+        Return the number of sample (forced as an int).
+
+    """
+    # The factor of 0.001 is to convert the value in milliseconds into seconds.
+    # This is done because a sample rate is given in seconds.
+    return int(ms * sample_rate * 0.001)
---
+++ tests/test_utils.py
@@ -0,0 +1,5 @@
+from asc import utils
+
+
+def test_ms2smp():
+    assert utils.ms2smp(1000, 44100) == 44100
