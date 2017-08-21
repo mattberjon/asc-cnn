e2cab0f79f74a7505e3b28966c019f62ea30f300
Matthieu Berjon <matthieu@berjon.net>
2017-08-21 21:36:30 +0200

    Adding a new set of tests
    I created few tests to `conf_param_extract()` and added new ones to
    `ms2smp()`. Nothing fancy but still better than nothing to ensure that
    the proper type of input is used and the expected result according to
    the input.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,21 @@
+import pytest
 from asc import utils
 
 
+def test_conf_param_extract():
+    with pytest.raises(TypeError):
+        utils.conf_param_extract(1000)
+
+    section, option = utils.conf_param_extract('audio.samplerate')
+    assert section == 'audio'
+    assert option == 'samplerate'
+
+
 def test_ms2smp():
+    with pytest.raises(TypeError):
+        utils.ms2smp('string', 44100)
+
+    with pytest.raises(TypeError):
+        utils.ms2smp(1000, 'string')
+
     assert utils.ms2smp(1000, 44100) == 44100
