04b6a042c3449cac09c17b635d935e0021efdcaa
Matthieu Berjon <matthieu@berjon.net>
2017-09-12 18:51:18 +0200

    Update of the tests
    I renamed and updated the tests for the audio process.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,10 +1,10 @@
-from asc import asc
+from asc import audio
 import pytest
 
 
 def test_spectrogram():
     with pytest.raises(AttributeError):
-        asc.spectrogram('string', 44100)
+        audio.spectrogram('string', 44100)
 
     with pytest.raises(AttributeError):
-        asc.spectrogram([1, 2, 3], 'string')
+        audio.spectrogram([1, 2, 3], 'string')
