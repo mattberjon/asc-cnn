ed5400cb0e562e2df39bb9a64bd5af87daa8a631
Matthieu Berjon <matthieu@berjon.net>
2017-11-09 12:07:36 +0100

    Adding of a default configuration for temporary folder
    I added in the default of the app configuration a temporary folder.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
+import tempfile
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class App:
     __conf = {
             'CONFIG_PATH': os.path.abspath(os.path.expanduser('~')) + '/.ascrc',
-            'URL_PATH': os.path.join(basedir, 'samples/url_list.txt')
+            'URL_PATH': os.path.join(basedir, 'samples/url_list.txt'),
+            'TMP_PATH': tempfile.gettempdir()
             }
 
     @staticmethod
